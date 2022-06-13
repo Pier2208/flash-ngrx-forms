258de91cb958293151925ffc344e0647565a9a31
april.outofbox@gmail.com <april.outofbox@gmail.com>
2022-06-13 16:50:12 -0400

    stuff

--- src/app/flashquote/flashquote.effects.ts
+++ src/app/flashquote/flashquote.effects.ts
@@ -3,8 +3,24 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { FlashquoteActions } from './actions/action-types';
 import { FlashquoteService } from './services/flashquote.service';
 import { FlashFormDTO } from './models/Flashquote';
-import { concatMap, map } from 'rxjs/operators';
+import { concatMap, map, switchMap } from 'rxjs/operators';
 import { FlashquoteLoadedAction } from './actions/flashquote.actions';
+import { AddGroupControlAction } from 'ngrx-forms';
+
+// @Injectable()
+// export class FlashquoteEffects {
+//   loadFlashquote$ = createEffect(() =>
+//     this.actions$.pipe(
+//       ofType(FlashquoteActions.loadFlashquote),
+//       concatMap((action) => this.flashquoteService.getFlashquote(action.marketId)),
+//       // concatMap((action) => this.flashquoteService.getFlashquote()),
+//       map((flashquote: any) => {
+//         // return new FlashquoteLoadedAction('FLASHQUOTE_LOADED', flashquote)
+//         localStorage.setItem("flash", JSON.stringify(flashquote))
+//           return new AddGroupControlAction('contracteur_v2', 'hello', '')
+//       })
+//     )
+//   );
 
 @Injectable()
 export class FlashquoteEffects {
@@ -13,14 +29,17 @@ export class FlashquoteEffects {
       ofType(FlashquoteActions.loadFlashquote),
       concatMap((action) => this.flashquoteService.getFlashquote(action.marketId)),
       // concatMap((action) => this.flashquoteService.getFlashquote()),
-      map((flashquote: FlashFormDTO ) => {
-        return new FlashquoteLoadedAction('FLASHQUOTE_LOADED', flashquote)
-      })
-    )
-  );
+      map((flashquote: any)=> {
+        return flashquote.questions.map((q: any) => {
+          return new AddGroupControlAction('contracteur_v2', q.id, '')
+        })
+      }),
+      switchMap((res: any) => [...res])
+  ))
+
 
   constructor(
     private actions$: Actions,
     private flashquoteService: FlashquoteService
-  ) {}
+  ) { }
 }
--- src/app/flashquote/reducers/formStateReducer.ts
+++ src/app/flashquote/reducers/formStateReducer.ts
@@ -1,4 +1,4 @@
-import { addGroupControl, createFormGroupState, createFormStateReducerWithUpdate, FormGroupState, removeGroupControl, updateGroup, validate } from "ngrx-forms";
+import { AddArrayControlAction, addGroupControl, AddGroupControlAction, createFormGroupState, createFormStateReducerWithUpdate, FormGroupState, removeGroupControl, updateGroup, validate } from "ngrx-forms";
 import { lessThanOrEqualTo, required } from "ngrx-forms/validation";
 import { Question } from "../models/Question";
 import { FormValue } from "../store";
@@ -8,99 +8,101 @@ export const FORM_ID = 'contracteur_v2';
 export const INITIAL_STATE = createFormGroupState<FormValue>(FORM_ID, {});
 
 export function formStateReducer(
-    s: FormGroupState<FormValue> | undefined = INITIAL_STATE,
-    a:
-      | FlashquoteLoadedAction
-      | CreateGroupElementAction
-      | RemoveGroupElementAction
-  ) {
-    switch (a.type) {
-      case FlashquoteLoadedAction.TYPE:
-        a.flashquote.questions.reduce((acc: any, question: Question) => {
-          if (question.isRequired) {
-            acc[question.id] = validate(required);
-          }
-          if (question.maxLength) {
-            acc[question.id] = validate(lessThanOrEqualTo(99));
-          }
+  s: FormGroupState<FormValue> | undefined = INITIAL_STATE,
+  a:
+    | FlashquoteLoadedAction
+    | CreateGroupElementAction
+    | RemoveGroupElementAction
+) {
+  console.log('action', a)
+  switch (a.type) {
+
+    case FlashquoteLoadedAction.TYPE:
+      a.flashquote.questions.reduce((acc: any, question: Question) => {
+        if (question.isRequired) {
+          acc[question.id] = validate(required);
+        }
+        if (question.maxLength) {
+          acc[question.id] = validate(lessThanOrEqualTo(99));
+        }
+        return acc;
+      }, {});
+
+      s = createFormGroupState<FormValue>(FORM_ID, {
+        2891: '',
+        1532: '',
+        1533: '',
+        2879: '',
+        2885: {},
+        2880: '',
+        2881: '',
+        2882: '',
+        2883: '',
+      });
+
+      return s;
+
+    case RemoveGroupElementAction.TYPE:
+      const newFormState = updateGroup<FormValue>({
+        [a.destinationId]: (group: any) => {
+          const newGroup = removeGroupControl(group, a.responseKey);
+          return newGroup;
+        },
+      })(s);
+
+      const withValidation = Object.keys(newFormState.controls).reduce(
+        (acc: any, key) => {
+          acc[key] = validate(required);
           return acc;
-        }, {});
-  
-        s = createFormGroupState<FormValue>(FORM_ID, {
-          2891: '',
-          1532: '',
-          1533: '',
-          2879: '',
-          2885: {},
-          2880: '',
-          2881: '',
-          2882: '',
-          2883: '',
-        });
-  
-        return s;
-  
-      case RemoveGroupElementAction.TYPE:
-        const newFormState = updateGroup<FormValue>({
+        },
+        {}
+      );
+
+      return createFormStateReducerWithUpdate<FormValue>(
+        updateGroup<FormValue>({
+          [a.destinationId]: updateGroup<any>(withValidation),
+        })
+      )(newFormState, a);
+
+    case CreateGroupElementAction.TYPE:
+      const value = s.controls[a.destinationId].value as {};
+      if (!(a.responseKey in value)) {
+        const newS = updateGroup<FormValue>({
           [a.destinationId]: (group: any) => {
-            const newGroup = removeGroupControl(group, a.responseKey);
-            return newGroup;
+            return addGroupControl(group, a.responseKey, '');
           },
         })(s);
-  
-        const withValidation = Object.keys(newFormState.controls).reduce(
+
+        const withValidation = Object.keys(newS.controls).reduce(
           (acc: any, key) => {
             acc[key] = validate(required);
             return acc;
           },
           {}
         );
-  
+
         return createFormStateReducerWithUpdate<FormValue>(
           updateGroup<FormValue>({
             [a.destinationId]: updateGroup<any>(withValidation),
           })
-        )(newFormState, a);
-  
-      case CreateGroupElementAction.TYPE:
-        const value = s.controls[a.destinationId].value as {};
-        if (!(a.responseKey in value)) {
-          const newS = updateGroup<FormValue>({
-            [a.destinationId]: (group: any) => {
-              return addGroupControl(group, a.responseKey, '');
-            },
-          })(s);
-  
-          const withValidation = Object.keys(newS.controls).reduce(
-            (acc: any, key) => {
-              acc[key] = validate(required);
-              return acc;
-            },
-            {}
-          );
-  
-          return createFormStateReducerWithUpdate<FormValue>(
-            updateGroup<FormValue>({
-              [a.destinationId]: updateGroup<any>(withValidation),
-            })
-          )(newS, a);
-        }
-  
-        return s
-  
-      default:
-        return createFormStateReducerWithUpdate<FormValue>(
-          updateGroup<FormValue>({
-            2891: validate(required),
-            1532: validate(required),
-            1533: validate(required),
-            2879: validate(required),
-            2885: updateGroup<any>({}),
-            2880: validate(required),
-            2881: validate(required),
-            2882: validate(required),
-            2883: validate(required),
-          })
-        )(s, a);
-    }
+        )(newS, a);
+      }
+
+      return s
+
+    default:
+      return createFormStateReducerWithUpdate<FormValue>(
+        updateGroup<FormValue>({
+          2891: validate(required),
+          1532: validate(required),
+          1533: validate(required),
+          2879: validate(required),
+          2885: updateGroup<any>({}),
+          2880: validate(required),
+          2881: validate(required),
+          2882: validate(required),
+          2883: validate(required),
+        })
+      )(s, a);
   }
+}
